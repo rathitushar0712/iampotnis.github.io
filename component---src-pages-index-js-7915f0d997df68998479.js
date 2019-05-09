61aa9556fa24c97af3157bd3881f8357fb632956
tusharrathi1993 <>
2019-05-09 22:31:31 +0530

    Changes

---
+++ component---src-pages-index-js-7915f0d997df68998479.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{140:function(n,e,o){"use strict";o.r(e);var t=o(0),i=o.n(t);e.default=function(){return i.a.createElement("div",null,"My portfolio!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-7915f0d997df68998479.js.map
